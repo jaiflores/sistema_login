8de9b6bd4f4432ef3537c8e4469167d7ef0cf60e
jaiflores <jaianny.vivian@gmail.com>
2022-11-09 16:07:54 -0300

    Mudança de banco de dados De sqlite3 para mysql

--- sistema_login/sistema_login/settings.py
+++ sistema_login/sistema_login/settings.py
@@ -76,9 +76,20 @@ WSGI_APPLICATION = 'sistema_login.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django2',
+        'USER': 'jaianny',
+        'PASSWORD': 'mini02@Dornelles12',
+        'HOST': '192.168.1.2',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
